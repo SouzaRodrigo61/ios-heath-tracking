5054966eb35340800fef9ac3b343f222d44f9097
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2020-07-25 12:36:44 -0300

    improviment: prepara o projeto para ambiente novo

--- life tracking/life tracking/Views/Home/HomeView.swift
+++ life tracking/life tracking/Views/Home/HomeView.swift
@@ -31,6 +31,10 @@ struct HomeView: View {
     @State private var bottomSheetShown = false
     
     
+    /// States - String
+    @State var state: String = ""
+    
+    
     /// Initializer
     func onInit() {
         self.getUser()
@@ -40,31 +44,27 @@ struct HomeView: View {
     
     // MARK: - Body
     var body: some View {
-        ZStack {
+        ZStack(alignment: .center) {
             MapRepresentable(pins: self.$pins, selectedPin: self.$selectedPin)
                 .edgesIgnoringSafeArea(.all)
-            VStack {
-                GeometryReader { (geometry) in
-                    VStack(alignment: .center) {
-                        HeaderComponent(user: self.$user)
-                    }
-                    .padding(.bottom, 10)
-                    .background(
-                        BlurRepresentable(style: .regular)
-                            .cornerRadius(10)
-                            .edgesIgnoringSafeArea(.top)
-                    )
+            VStack(alignment: .center) {
+                GeometryReader { geometry in
+                    
+                    /// - Home Header
+                    HeaderView(user: self.$user, state: self.$state)
+                    
                     Spacer()
                     
-                    if (self.user.name != "") {
-                        HomeBottomSheet(bottomSheetShown: self.$bottomSheetShown, geometry: geometry, user: self.$user)
-                    }
+                    /// - Home Bottom
+                    BottomView(user: self.$user, geometry: geometry)
+                    
                 }
             }
             .onAppear(perform: onInit)
         }
     }
 }
+
 
 // MARK: - HomeView Previews
 struct HomeView_Previews: PreviewProvider {
@@ -74,3 +74,95 @@ struct HomeView_Previews: PreviewProvider {
     }
 }
 
+
+// MARK: - Bottom View
+fileprivate struct BottomView: View {
+    
+    /// States - Person
+    @Binding var user: Person
+    
+    /// States - Boolean
+    @State private var bottomSheetShown = false
+    
+    
+    var geometry: GeometryProxy
+    
+    
+    var body: some View {
+        VStack {
+            if (self.user.name != "") {
+                HomeBottomSheet(bottomSheetShown: self.$bottomSheetShown, geometry: geometry, user: self.$user)
+            } else {
+                HStack() {
+                    Spacer()
+                    Button(action: {}) {
+                        VStack {
+                            HStack {
+                                Image(systemName: "person.crop.circle.badge.exclam")
+                                Text("Você voltou ?")
+                            }
+                            .padding()
+                        }
+                        .onTapGesture {
+                            print("on tap")
+                        }
+                        .background(
+                            BlurRepresentable(style: .regular)
+                                .cornerRadius(10)
+                        )
+                    }
+                    Spacer()
+                }
+                .padding(.top, geometry.size.height - geometry.safeAreaInsets.bottom - 20)
+            }
+        }
+    }
+}
+
+
+// MARK: - Header View
+fileprivate struct HeaderView: View {
+    @Binding var user: Person
+    @Binding var state: String
+    
+    var body: some View {
+        VStack {
+            
+            
+            /// - Header
+            VStack(alignment: .center) {
+                HeaderComponent(user: self.$user)
+                
+                
+            }
+            .padding(.bottom, 10)
+            .background(
+                BlurRepresentable(style: .regular)
+                    .cornerRadius(10)
+                    .edgesIgnoringSafeArea(.top)
+            )
+            
+            /// - Procurar o estado
+            VStack {
+                VStack {
+                    HStack {
+                        Image(systemName: "location.fill")
+                            .foregroundColor(Color.secondary)
+                        TextField("Seleciona o estado", text: self.$state)
+                            .padding(.leading, 5)
+                    }
+                    .padding()
+                }
+                .background(
+                    BlurRepresentable(style: .regular)
+                        .cornerRadius(10)
+                        .edgesIgnoringSafeArea(.top)
+                )
+            }
+            .padding(.top, 5)
+            .padding(.horizontal, 20)
+            
+            
+        }
+    }
+}
